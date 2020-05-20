ca59cc7d634adec429d5c12fb219cbfb12135165
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-05-20 10:14:09 -0300

    Create Gerenciamento de Notas.c

---
+++ Lista 2 e Gerenciamento de Notas/Gerenciamento de Notas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void){
+	
+	int aux;
+	
+	printf("1-Criar turma\n2-Procurar aluno\n3-Gerar estatisticas das turmas\n4-Gravar informacoes da turma\n5-Encerrar sistema");
+	scanf("%d",&aux);
+	
+	switch (aux){
+		case 1:			//Criar Turma
+			break;
+		case 2:			//Procurar Aluno
+			break;
+		case 3:			//Gerar estatisticas de Turma
+			break;
+		case 4:			//Gravar informações de Turma
+			break;
+		case 5:			//Encerar sistema
+			break;
+	}
+	
+}
