d5a4832c8d9242272a6fb1e02925e96da7e8c50d
WilliamOrl <62764335+WilliamOrl@users.noreply.github.com>
2020-05-22 17:20:30 -0300

    Update Gerenciamento de Notas.c

--- Lista 2 e Gerenciamento de Notas/Gerenciamento de Notas.c
+++ Lista 2 e Gerenciamento de Notas/Gerenciamento de Notas.c
@@ -18,17 +18,21 @@ typedef struct{
 
 typedef struct{
 	classe* turma;
+	int N_de_alunos;
 }sala;
 
 //====================================================================
 //		Lista de funções
 //====================================================================
 
-void criar_turma(/*classe* aluno,*/ sala* pessoas,int n_t);
+void criar_turma(sala* pessoas,int n_t);
 int media(float n1, float n2, float tra, int n);
 void notas(float *n1,float *n2,float *tra, int i);
 void informando(char *nome, char *sobre,int *usp,int i);
-void mostra(sala* pessoas);
+void Gerar_estatisticas(sala* pessoas,int n_t);
+float Media_P1(sala* pessoas, int i);
+float Media_P2(sala* pessoas, int i);
+
 //====================================================================
 //		Inicio
 //====================================================================
@@ -37,7 +41,6 @@ int main(void){
 	
 	int aux,n_t=0,i=0;
 	int cod_turma[10];
-	//classe *aluno;
 	sala pessoas[20];
 	
 	while(1){
@@ -57,15 +60,14 @@ int main(void){
 		
 		switch(aux){
 			case 1:
-				criar_turma(/*aluno,*/pessoas,n_t);
-			//	pessoas[n_t].turma = aluno;
+				criar_turma(pessoas,n_t);
 				n_t++;
-			//	mostra(pessoas);
 				break;
 			case 2:
 			//	procurar_aluno(aluno);
 				break;	
 			case 3:
+				Gerar_estatisticas(pessoas, n_t);
 				break;
 			case 4:
 				break;
@@ -78,27 +80,12 @@ fim:
 	return 0;
 
 }
-/*
-void mostra(sala* pessoas) {
-	int i;
-	printf("a");
-	for(i=0;i!=2;i++){
-    			printf("Numero USP:\n %d\n", pessoas[0].turma[i].num_usp);
-    			printf("Nome completo:\n %s %s\n", pessoas[0].turma[i].nome, pessoas[0].turma[i].sob);
-    			printf("Primeira nota:\n %.2f\n", pessoas[0].turma[i].P1);
-    			printf("Segunda nota:\n %.2f\n", pessoas[0].turma[i].P2);
-    			printf("Nota no Trabalho:\n %.2f\n", pessoas[0].turma[i].N_tab);
-    			printf("Media final:\n %.2f\n", pessoas[0].turma[i].med_fim);
-  			}
-  	getchar();
-}
-*/
 
 //====================================================================
 //		Criar Turma
 //====================================================================
 
-void criar_turma(/*classe* aluno,*/ sala* pessoas, int n_t){
+void criar_turma(sala* pessoas, int n_t){
 	int n,i, *usp;
 	float *n1, *n2, *tra;
 	
@@ -106,8 +93,8 @@ void criar_turma(/*classe* aluno,*/ sala* pessoas, int n_t){
 	printf("Digite o numero de Alunos: ");
 	scanf("%d", &n);
 
-	//aluno = (classe *)malloc(n*sizeof(classe));
 	pessoas[n_t].turma = (classe *)malloc(n*sizeof(classe));
+	pessoas[n_t].N_de_alunos = n;
 	
 	for(i=0;i!=n;i++){
 		
@@ -183,3 +170,75 @@ void informando(char *nome, char *sobre,int *usp,int i){
   scanf("%d", &*usp);
 }
 
+//====================================================================
+//		Cria Estatisticas das Turmas
+//====================================================================
+
+void Gerar_estatisticas(sala* pessoas, int n_t){
+	
+	if(n_t == 0){
+		printf("Nenhuma turma registrada");
+	}
+	
+	else{
+	
+		int i;	
+		float med_P1[n_t];
+		float med_P2[n_t];
+		float med_trab[n_t];
+		
+		for(i=0;i!=n_t;i++){
+			med_P1[i] = Media_P1(pessoas,i);
+			med_P2[i] = Media_P2(pessoas,i);
+		}
+		
+		for(i=0;i!=n_t;i++){
+			printf(" Turma %d         ",i+1);
+		}
+			
+		printf("\n");
+		
+		for(i=0;i!=n_t;i++){
+			printf("Medias P1 %.2f ",med_P1[i]);
+		}			
+		
+		printf("\n");
+		
+		for(i=0;i!=n_t;i++){
+			printf("Medias P2 %.2f ",med_P2[i]);
+		}			
+				
+	}
+	sleep(10);	
+}
+
+//====================================================================
+//		Media da P1
+//====================================================================
+
+float Media_P1(sala* pessoas, int i){
+	
+	int j;
+	float aux=0;
+	
+	for(j=0;j!=pessoas[i].N_de_alunos;j++){
+		aux = aux + pessoas[i].turma[j].P1;	
+	}
+	aux = aux/pessoas[i].N_de_alunos;
+	return aux;
+}
+
+//====================================================================
+//		Media da P2
+//====================================================================
+
+float Media_P2(sala* pessoas, int i){
+	int j;
+	float aux=0;
+	
+	for(j=0;j!=pessoas[i].N_de_alunos;j++){
+		aux = aux + pessoas[i].turma[j].P2;	
+	}
+	aux = aux/pessoas[i].N_de_alunos;
+	return aux;
+}
